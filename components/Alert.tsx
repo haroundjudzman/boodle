89de85b758be14f05caf1cb9999f873aa8901ccb
Haroun Djudzman <haroun.djudzman@gmail.com>
2022-02-05 23:28:32 +0700

    fix alert style

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -4,7 +4,7 @@ export default function Alert({ children }: { children: ReactNode }) {
   return (
     <div
       role="alert"
-      className="absolute-center bg-white text-black text-center font-semibold py-2 px-3 rounded-sm"
+      className="absolute-center bg-red-500 text-black text-center font-semibold py-2 px-3 rounded-sm"
     >
       {children}
     </div>
